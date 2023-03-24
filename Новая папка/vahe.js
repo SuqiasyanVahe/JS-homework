0123c8bcadec75749d6a43c6264436ef1eff6e00
SuqiasyanVahe <123183082+SuqiasyanVahe@users.noreply.github.com>
2023-03-24 21:26:02 +0400

    Add files via upload

---
+++ Новая папка/vahe.js
@@ -0,0 +1,30 @@
+let vahe = 14
+
+let age1 = 10
+let age2 = 99
+
+if (age1 < vahe < age2) {
+    alert('tru')
+} else{
+    alert('false')
+}
+
+let a = 1
+let b = 9
+
+if (a + b == 10) {
+    alert('you are matematic')
+} else {
+    alert('you are not matematic')
+}
+'Hrazdan' == 'Hrazdan' ? a = 4 :a = 1
+4
+'Hrazdan' == 'Qajaran' ? a = 4 :a = 1
+1
+let my_country = 'Hayastan'
+
+if (my_country = 'USA') {
+    alert('NO')
+} else if (my_country = 'Hayastan') {
+    alert('YES')
+}
